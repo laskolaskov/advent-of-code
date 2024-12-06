e968b4fa8bb706212e426906cf63134dbee4de6d
Lasko Laskov <laskov.lasko@gmail.com>
2024-12-06 17:15:41 +0200

    2024 day 6 part 1

--- aoc2024/day6.go
+++ aoc2024/day6.go
@@ -7,20 +7,70 @@ import (
 )
 
 func Day6part1() int {
-	parseInputDay6()
-	return 0
+	m, pos := parseInputDay6()
+	h := len(m)
+	w := len(m[0])
+
+	//start by going up
+	d := complex(-1, 0)
+
+	//visited
+	v := map[complex128]bool{}
+
+	//mark start as visited
+	v[pos] = true
+
+	for {
+		//get next tile coords
+		n := pos + d
+		//get cords as integers
+		ni := int(real(n))
+		nj := int(imag(n))
+		//check if out of map
+		if ni < 0 || nj < 0 || ni >= h || nj >= w {
+			break
+		}
+		//check tile type
+		switch string(m[ni][nj]) {
+		case "#":
+			//change direction: always turn right by 90deg
+			d = d * complex(0, -1)
+		case ".":
+			//move in the direction and mark as visited
+			pos = n
+			v[pos] = true
+		default:
+			//move in the direction without marking as visited
+			pos = n
+		}
+	}
+
+	return len(v)
 }
 
-func parseInputDay6() {
-	file, err := os.Open("./aoc2024/input-day5test.txt")
+func parseInputDay6() ([]string, complex128) {
+	file, err := os.Open("./aoc2024/input-day6.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer file.Close()
 
+	var p complex128
+	m := []string{}
+
 	scanner := bufio.NewScanner(file)
 
+	i := 0
 	for scanner.Scan() {
-		//l := scanner.Text()
+		l := scanner.Text()
+		m = append(m, l)
+		for j, c := range l {
+			if c == '^' {
+				p = complex(float64(i), float64(j))
+			}
+		}
+		i++
 	}
+
+	return m, p
 }
